1f33a5015e109c6b6a5be9c72585aeb66e1d89fb
IdoBar <idobar100@hotmail.com>
2026-04-21 18:46:06 +0300

    added logic for addLecturer

--- javaProject/src/Main.java
+++ javaProject/src/Main.java
@@ -5,13 +5,23 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         int choice;
+
+        String[] lecturers = new String[2];
+        String[] depts = new String[2];
+        String[] comms = new String[2];
+
+        int lecSize = 0;
+        int commsSize = 0;
+        int deptsSize = 0;
 
         System.out.println("Name you college...");
         String college = scanner.nextLine();
         System.out.println("Your college is: " + college);
 
         do {
+
             System.out.println("--- ACADEMIC STAFF MANAGEMENT SYSTEM ---");
             System.out.println("0: Exit.");
             System.out.println("1: Add Lecturer.");
@@ -30,20 +40,30 @@ public class Main {
                 case 0:
                     break;
                 case 1:
+                    System.out.println("Provide lecturer name...");
+                    String newLecturer = scanner.nextLine();
+                    lecturers = addLecturer(newLecturer, lecturers, lecSize);
                     break;
                 case 2:
+                    lecturers = addCommittee();
                     break;
                 case 3:
+                    addDept();
                     break;
                 case 4:
+                    assignLecturer();
                     break;
                 case 5:
+                    WIP();
                     break;
                 case 6:
+                    WIP();
                     break;
                 case 7:
+                    viewLecturerDetails();
                     break;
                 case 8:
+                    viewDeptsDetails();
                     break;
             }
 
@@ -52,15 +72,20 @@ public class Main {
         scanner.close();
     }
 
-    private static void addLecturer() {
+    private static String[] addLecturer(String newLecturer, String[] lecturers, int lecSize) {
+        if (lecSize == lecturers.length) {
+            lecturers = doubleArray(lecturers);
+        }
+
+        lecturers[lecSize] = newLecturer;
+        return lecturers;
+    }
+
+    private static String[] addCommittee() {
 
     }
 
-    private static void addCommittee() {
-
-    }
-
-    private static void addDept() {
+    private static String[] addDept() {
 
     }
 
@@ -78,5 +103,18 @@ public class Main {
 
     private static void viewDeptsDetails() {
 
+    }
+
+    private static String[] doubleArray(String[] arr) {
+        int arrExtFactor = 2;
+        int arrSize = arr.length;
+
+        String[] newArr = new String[arrExtFactor * arrSize];
+
+        for (int i = 0; i < arrSize; i++) {
+            newArr[i] = arr[i];
+        }
+
+        return newArr;
     }
 }
