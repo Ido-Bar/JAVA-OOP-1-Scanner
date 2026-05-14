424ab44f08f1087d848fb5878a44b589a4bfe9e7
Shaked Govrin <shakedgo@gmail.com>
2026-05-14 21:04:45 +0300

    Started refactir

---
+++ javaProject/src/Manager.java
@@ -0,0 +1,11 @@
+public class Manager {
+    String[] lecturers = new String[2];
+    String[] depts = new String[2];
+    String[] comms = new String[2];
+
+    int lecSize = 0;
+    int commsSize = 0;
+    int deptsSize = 0;
+
+
+}
